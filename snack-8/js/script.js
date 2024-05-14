fc537f0c2ed8df6ad97967394aa7addd1094331b
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-14 11:44:35 +0200

    created div elements and array

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -1,1 +1,7 @@
 //todo: Crea con js tre tag div con tre id diversi. Un div avrà il testo colorato di rosso, uno di verde e l'altro di giallo. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari, in verde i numeri pari, in giallo i multipli di 5.
+
+// <-- creo i div
+const mainEl = document.querySelector('main');
+mainEl.innerHTML = '<div id=\'div-red\'></div> <div id=\'div-green\'></div> <div id=\'div-yellow\'></div>';
+
+const numbersArray = [2, 5, 34, 25, 64, 61, 3, 22, 45, 80, 13];
