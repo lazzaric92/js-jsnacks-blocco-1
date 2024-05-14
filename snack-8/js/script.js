31dff93f07e8c24c47a92cc64f8cdb340e73a4ce
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-14 11:52:43 +0200

    created for cycle

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -5,3 +5,17 @@ const mainEl = document.querySelector('main');
 mainEl.innerHTML = '<div id=\'div-red\'></div> <div id=\'div-green\'></div> <div id=\'div-yellow\'></div>';
 
 const numbersArray = [2, 5, 34, 25, 64, 61, 3, 22, 45, 80, 13];
+
+for (let i = 0; i < numbersArray.length; i++){
+    const yellowDiv = document.getElementById('div-yellow');
+    const redDiv = document.getElementById('div-red');
+    const greenDiv = document.getElementById('div-green');
+
+    if (numbersArray[i] % 5 === 0){
+        yellowDiv.append(numbersArray[i] + ', ');
+    } else if (numbersArray[i] % 2 === 1){
+        redDiv.append(numbersArray[i] + ', ');
+    } else {
+        greenDiv.append(numbersArray[i] + ', ');
+    }
+}
